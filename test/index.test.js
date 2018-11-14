e0a08908126e01b22de8587fd564dc36008bf9d0
Ash Wilson <smashwilson@github.com>
2018-11-14 14:14:00 -0500

    Controllably flaking test

--- test/index.test.js
+++ test/index.test.js
@@ -9,4 +9,12 @@ describe('the test suite', function() {
   it('fails if LOOK_A_FAILURE is set', function() {
     assert.strictEqual(process.env.LOOK_A_FAILURE || '', '');
   })
+
+  it('fails sporadically if FLAKE_IT_GOOD is set', function() {
+    if (process.env.FLAKE_IT_GOOD && Math.random() > 0.5) {
+      assert.isFalse(true, 'oh no a flake');
+    } else {
+      assert.isTrue(true);
+    }
+  })
 })
